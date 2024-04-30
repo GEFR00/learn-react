65df7aeee3ae71696c1bce88a97f177460c8b857
GEFR00 <elizabehtriquelme@gmail.com>
2024-04-30 01:18:16 -0400

    [ADD] componentes: lista tareas, sección tareas realizadas, CRUD tareas

--- projects/03-to-do-list/src/App.jsx
+++ projects/03-to-do-list/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { CreateTaskList } from './components/CreateTaskList'
+import { TaskList } from './components/TaskList';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [taskList, setTaskList] = useState([])
+  const [editingIndex, setEditingIndex] = useState(null);
+
+  const createTask = (task) => {
+
+    if(editingIndex !== null) {
+      setTaskList(taskList.map((item, index) => index === editingIndex ? task : item));
+      setEditingIndex(null);
+    } else {
+      setTaskList([...taskList, task]);
+    }
+  }
+
+  const deleteTask = (index) => {
+    const newTaskList = taskList.filter((_, i) => i !== index);
+    setTaskList(newTaskList);
+  }
+
+  const handleEditIndexTask = (index) => {
+    setEditingIndex(index);
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <main className='App'>
+        <h1>To-do-List</h1>
+
+        <CreateTaskList
+          createTask={createTask}/>
+
+        <TaskList
+          taskList={taskList} 
+          deleteTask={deleteTask} 
+          createTask={createTask}
+          handleEditIndexTask={handleEditIndexTask}>
+            { editingIndex !== null && <CreateTaskList createTask={createTask}/> }
+        </TaskList>
+     </main>
     </>
   )
 }
